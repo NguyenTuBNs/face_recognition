afe3a9b9611743e413ca13affb9ae5797cf586c3
NguyenTuBNs <anhtu.develop@gmail.com>
2026-06-06 19:38:32 +0700

    initial comit

---
+++ test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import sklearn
+
+print("TensorFlow:", tf.__version__)
+print("OpenCV:", cv2.__version__)
+print("Numpy:", np.__version__)
+print("Tất cả OK!")
